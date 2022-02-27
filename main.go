2b1464698b3f04d92eb9424962fa9d4d128a92db
Junghee Shim <jhee086@gmail.com>
2022-02-27 17:26:55 +0900

    final main.go(Job Scrapper)

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
-/* Job Scrapper
-1. getPages 함수 실행 (총 몇 페이지인지 알 수 있음)
-2. 각 페이지 별로 getPage 실행
-   => 모든 페이지를 동시에 요청하도록 goroutine 생성
-3. getPage 실행되는 중에 extractJob도 실행됨 (각 페이지에는 50개의 일자리 정보 존재)
-   => getPage안에 실행되는 extractJob 50개를 동시에 실행하도록 goroutine 생성
-   => 요청된 정보는 channel 기능 이용하여 정보를 주고 받음 (main함수와)
-4. getPage 실행 종료 후 main함수로 채널 전송
-
-Challenge: 파일 쓰기 부분 goroutine으로
+/*
+Job Scrapper
+1. Run the getPages function (you can find out how many pages there are in total)
+2. Execute getPage for each page
+   => Create a goroutine to request all pages at the same time
+3. While getPage is running, extractJob is also executed (each page has 50 job information)
+    => Create goroutine to simultaneously execute 50 extractJobs executed in getPage
+    => The requested information is exchanged using the channel function (with the main function)
+4. After the execution of getPage is finished, the channel is sent to the main function.
 */
 
 import (
@@ -32,34 +31,32 @@ type extractedjob struct {
 	summary string
 }
 
-var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+var baseURL string = "https://kr.indeed.com/jobs?q=data&limit=50"
 
 func main() {
-	// 많은 배열들의 조합 [] + [] + [] ...
+	// combination of arrays [] + [] + [] ...
 	var jobs []extractedjob
 	// channel
 	c := make(chan []extractedjob)
 
 	/* scrapper project
-	: indeed site -> /job/ search -> how many pages
-	-> changes start time -> get first page  */
+	: indeed site -> /job/ search -> how many pages -> changes start time -> get first page */
 	// 1. go get github.com/PuerkitoBio/goquery
 	// 2. get all indeed page
 	totalPages := getPages()
 	// fmt.Println(totalPages)
 	// 3. go query : getPages()
 	// 4. hit URL
-	// 총 페이지 숫자를 확인한 후 각 페이지별로 getPage함수 호출
+	// After checking the total number of pages, call the getPage function for each page
 	for i := 0; i < totalPages; i++ {
 		go getPage(i, c)
 		// extractedJobs := getPage(i)
-		// // ... : 하나의 배열로 합치는 것
+		// // ... : merging into one array
 		// jobs = append(jobs, extractedJobs...)
 	}
-	// 몇개의 goroutine을 기다려야 할까?
-	// : 총 n개의 페이지 조회되면 n개의 goroutine
+	// How many goroutines should we wait for? -> When a total of n pages are viewed, n goroutines
 	for i := 0; i < totalPages; i++ {
-		// 메시지 기다리는 중
+		// waiting for message
 		extractedJobs := <-c
 		jobs = append(jobs, extractedJobs...)
 	}
@@ -67,46 +64,69 @@ func main() {
 	writeJobs(jobs)
 	fmt.Println("Done, extracted", len(jobs))
 
-} // main 끝
+} // end of main
 
-// csv로 저장
+// // save as csv
+// func writeJobs(jobs []extractedjob) {
+// 	file, err := os.Create("jobs.csv")
+// 	checkErr(err)
+// 	w := csv.NewWriter(file)
+// 	// Flush function: put everything written to a file
+// 	// A function that stores job information by passing it to the writer
+// 	defer w.Flush()
+
+// 	headers := []string{"Link", "Title", "Location", "Summary"}
+// 	wErr := w.Write(headers)
+// 	checkErr(wErr)
+
+// 	// All pages imported jobs are entered. Execute defer w.Flush() after for.
+// 	for _, job := range jobs {
+// 		jobSlice := []string{"https://kr.indeed.com/viewjob?jk=" + job.id, job.title, job.location, job.summary}
+// 		jwErr := w.Write(jobSlice)
+// 		checkErr(jwErr)
+// 	}
+// }
+
+// Challenge -> make a goroutine (Is it more fast?)
 func writeJobs(jobs []extractedjob) {
 	file, err := os.Create("jobs.csv")
 	checkErr(err)
 	w := csv.NewWriter(file)
-	// Flush 함수: 작성된 모든 것들을 파일에 입력
-	// 일자리 정보가 writer로 전달되어서 저장하는 함수
 	defer w.Flush()
 
 	headers := []string{"Link", "Title", "Location", "Summary"}
 	wErr := w.Write(headers)
 	checkErr(wErr)
 
-	// 모든 페이지에서 가져온 jobs가 입력, for가 끝나고 defer w.Flush() 실행
-	// Challenge : goroutine 만들어보기
+	c := make(chan []string)
 	for _, job := range jobs {
-		jobSlice := []string{"https://kr.indeed.com/viewjob?jk=" + job.id, job.title, job.location, job.summary}
-		jwErr := w.Write(jobSlice)
-		checkErr(jwErr)
+		go func(job extractedjob, c chan<- []string) {
+			c <- []string{"https://kr.indeed.com/viewjob?jk=" + job.id, job.title, job.location, job.summary}
+		}(job, c)
+	}
+	for i := 0; i < len(jobs); i++ {
+		jobData := <-c
+		writeErr := w.Write(jobData)
+		checkErr(writeErr)
 	}
 }
 
 func cleanString(str string) string {
-	// Fields: space를 기준으로 배열안에 텍스트를 넣음 -> 반환값: []string
-	// Join: 배열을 가져와서 합치는 역할
+	// Fields: Inserts text into an array based on space -> Return value: []string
+	// Join: Taking an array and concatenating it
 	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
-// 각 페이지에 있는 일자리를 모두 반환하는 함수
-// getPage는 중간다리 - goroutine을 생성해서 일자리를 전달하고 main함수의 채널로 전송
+// Function to return all jobs on each page
+// getPage(an intermediate bridge) - make a goroutine, passes the jobs, and sends jobs to the main function's channel.
 func getPage(page int, mainC chan<- []extractedjob) {
 	var jobs []extractedjob
 	// channel
 	c := make(chan extractedjob)
-	// 필요한 주소 만들기
+	// create a pageUrl
 	pageUrl := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Requesting", pageUrl)
-	// 정보가져오는 요청
+	// Request to get information
 	res, err := http.Get(pageUrl)
 	checkErr(err)
 	checkCode(res)
@@ -116,30 +136,30 @@ func getPage(page int, mainC chan<- []extractedjob) {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	// job card에서 일자리 정보 가져오기
+	// Get job information from job card
 	searchCards := doc.Find(".tapItem")
 	searchCards.Each(func(i int, card *goquery.Selection) {
-		// id, _ := card.Attr("data-jk") // id 찾기
+		// id, _ := card.Attr("data-jk") // find a id
 		// // fmt.Println(id)
 		// title := cleanString(card.Find("h2>span").Text())
 		// // fmt.Println(title)
 		// location := cleanString(card.Find(".companyLocation").Text())
 		// fmt.Println(id, title, location)
-		// extractedJob struct를 반환 후 job변수에 저장
-		go extractedJob(card, c) // 각각의 카드에 대해서 job을 추출
-		// jobs = append(jobs, job)     // jobs에 저장
+		// After returning the extractedJob struct, it is stored in the job variable.
+		go extractedJob(card, c) // Extract a job for each card
+		// jobs = append(jobs, job)     // save to jobs
 	})
-	// extractJob goroutine에서 정보가 전달되면 jobs를 mainC로 전달
+	// Passing jobs to mainC when information is passed from extractJob goroutine
 	for i := 0; i < searchCards.Length(); i++ {
 		job := <-c
 		jobs = append(jobs, job)
 	}
-	// mainC(채널)에는 []extractedjob가 입력됨
+	// []extractedjob is entered in mainC (channel)
 	mainC <- jobs
 }
 
 func extractedJob(card *goquery.Selection, c chan<- extractedjob) {
-	id, _ := card.Attr("data-jk") // id 찾기
+	id, _ := card.Attr("data-jk") // find a id
 	title := cleanString(card.Find("h2>span").Text())
 	location := cleanString(card.Find(".companyLocation").Text())
 	// salary :=
@@ -153,7 +173,7 @@ func extractedJob(card *goquery.Selection, c chan<- extractedjob) {
 	}
 }
 
-// 페이지의 총 숫자 찾는 함수
+// Function to find the total number of pages
 func getPages() int {
 	pages := 0
 	res, err := http.Get(baseURL)
@@ -165,7 +185,7 @@ func getPages() int {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	// 클래스 찾기 fmt.Println(doc) // &{0xc0004604e0 <nil> 0xc000206000}
+	// find a class,  fmt.Println(doc) // &{0xc0004604e0 <nil> 0xc000206000}
 	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
 		// fmt.Println(s.Html())
 		pages = s.Find("a").Length()
